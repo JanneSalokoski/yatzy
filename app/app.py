c2ec67f786865b7161dbe6c10660ce49d63e687f
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-08 12:42:31 +0300

    feat(db): use database in a reusable way

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, g
 
 import os
 import psycopg2
@@ -6,21 +6,41 @@ import psycopg2
 app = Flask(__name__)
 
 
+def get_db():
+    if "db" not in g:
+        g.db = psycopg2.connect(os.environ["DATABASE_URL"])
+
+    return g.db
+
+
+def db_cursor(func):
+    def wrapper(*args, **kwargs):
+        conn = get_db()
+        cur = conn.cursor()
+        res = func(*args, **kwargs, cur=cur)
+        cur.close()
+        return res
+
+    return wrapper
+
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+
 @app.route("/")
 def index():
     return render_template("home.html", page_title="Etusivu")
 
 
+@db_cursor
 @app.route("/status")
-def status():
-    conn = psycopg2.connect(os.environ["DATABASE_URL"])
-    cur = conn.cursor()
-
+def status(cur):
     cur.execute("SELECT version();")
     db_version = cur.fetchone()
-
-    cur.close()
-    conn.close()
 
     return f"Connected to DB: {db_version[0]}"
 
