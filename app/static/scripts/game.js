10670eabd10692f2c820a0b65d583bf8945f271a
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-09 13:06:06 +0300

    feat(game): display statistics about the thrown dice

--- app/static/scripts/game.js
+++ app/static/scripts/game.js
@@ -1,3 +1,11 @@
+let die_values = {
+    "die-1": { value: 0, locked: false },
+    "die-2": { value: 0, locked: false },
+    "die-3": { value: 0, locked: false },
+    "die-4": { value: 0, locked: false },
+    "die-5": { value: 0, locked: false },
+};
+
 async function load_svg(face) {
     const res = await fetch(`/static/svg/die-${face}.svg`);
     return await res.text();
@@ -38,10 +46,11 @@ async function throw_die(die, faces) {
 
     die.classList.add("rolling");
 
+    let value = 1;
     for (let i = 0; i < die_roll_amount(); i++) {
-        const value = die_throw();
-        die.innerHTML = faces[value - 1];
+        value = die_throw();
 
+        die.innerHTML = faces[value - 1];
         const svg = die.querySelector("svg");
 
         await delay(die_roll_time() * 10);
@@ -49,6 +58,7 @@ async function throw_die(die, faces) {
         svg.classList.add("fade-in");
     }
 
+    die_values[die.id].value = value;
     die.classList.remove("rolling");
 }
 
@@ -64,20 +74,51 @@ function get_dice() {
     dice.forEach(die => {
         die.addEventListener("click", () => {
             die.classList.toggle("selected");
+            die_values[die.id].locked = !die_values[die.id].locked;
+            update_stats();
         });
     });
 
     return dice;
 }
 
+function update_stats() {
+    const sum_element = document.querySelector(".sum .value");
+    const locked_sum_element = document.querySelector(".locked-sum .value");
+    const max_sum_element = document.querySelector(".max-sum .value");
+
+    const sum = Object.values(die_values)
+        .map(val => val.value)
+        .reduce((total, val) => total + val, 0);
+
+    const locked_sum = Object.values(die_values)
+        .filter(v => v.locked === true)
+        .map(val => val.value)
+        .reduce((total, val) => total + val, 0);
+
+    const free = Object.values(die_values)
+        .filter(v => v.locked === false)
+        .length;
+
+    const max_sum = locked_sum + free * 6;
+
+    sum_element.textContent = sum;
+    locked_sum_element.textContent = locked_sum;
+    max_sum_element.textContent = max_sum;
+}
+
 async function init() {
     const die_faces = await Promise.all(load_die_faces());
     const dice = get_dice();
 
+    update_stats();
+
     const throw_button = document.getElementById("throw");
-    throw_button.onclick = (e) => {
+    throw_button.onclick = async (e) => {
         e.preventDefault();
-        dice.forEach(die => throw_die(die, die_faces));
+
+        await Promise.all(dice.map(die => throw_die(die, die_faces)));
+        update_stats();
     }
 }
 
